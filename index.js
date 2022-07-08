4d276eee80151c43bbe976d342903ddb30f2ec58
dkkim0122 <dkkim0122@gmail.com>
2022-07-08 18:17:18 +0900

    데이터 스타일 바인딩까지 완료

--- index.js
+++ index.js
@@ -1,76 +1,8 @@
-Vue.component('todo-item', {
-  props: ['todo'],
-  template: '<li>{{todo.text}}</li>',
-})
-
-var app = new Vue({
+var vm = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!',
-  },
-})
-app.message = 'Hello again!'
-
-var app2 = new Vue({
-  el: '#app-2',
-  data: {
-    message: 'Loaded!',
-  },
-})
-app2.message = 'hello~'
-
-var app3 = new Vue({
-  el: '#app-3',
-  data: {
-    seen: true,
-    buttonTitle: '메세지 감추기',
-  },
-  methods: {
-    showMessage: function () {
-      if (this.seen) {
-        this.seen = false
-        this.buttonTitle = '메세지 보이기'
-      } else {
-        this.seen = true
-        this.buttonTitle = '메세지 감추기'
-      }
-    },
-  },
-})
-
-var app4 = new Vue({
-  el: '#app-4',
-  data: {
-    todos: [{ text: 'javascript' }, { text: 'react' }, { text: 'eveything!' }],
-  },
-})
-
-var app5 = new Vue({
-  el: '#app-5',
-  data: {
-    message: 'Hello, I am John!',
-  },
-  methods: {
-    reverseMessage: function () {
-      this.message = this.message.split('').reverse().join('')
-    },
-  },
-})
-
-var app6 = new Vue({
-  el: '#app-6',
-  data: {
-    message: '',
-  },
-})
-
-var app7 = new Vue({
-  el: '#app-7',
-  data: {
-    groceryList: [
-      { id: 0, text: 'Watermelon' },
-      { id: 1, text: 'Apple' },
-      { id: 2, text: 'Banana' },
-    ],
+    activeClass: 'active',
+    errorClass: 'text-danger',
+    isActive: false,
   },
 })
